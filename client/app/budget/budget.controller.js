3bc9fcaeba6b8166d230dba7280c50b919e0e64d
Arne Pfeilsticker <Arne.Pfeilsticker@piratenpartei-hessen.de>
2015-08-14 11:15:09 +0200

    Update budget.controller.js

--- client/app/budget/budget.controller.js
+++ client/app/budget/budget.controller.js
@@ -11,23 +11,22 @@
 
         vm.personId = 'de.05315000';
 
-        vm.yearDimensionData = {};
-        //vm.prod1Dimension = {};
-        //vm.prod2Dimension = {};
-        //vm.prod3Dimension = {};
-        //vm.prod1SumGroup = {};
-        //vm.prod2SumGroup = {};
-        //vm.prod3SumGroup = {};
-        //vm.paymentDimension = {};
-        //vm.paymentGroup = {};
-        //vm.paymentRangeChartOptions = {};
-        //vm.paymentsOutSumGroup = {};
-        //vm.paymentsSumGroup = {};
-        //vm.setupRangeChart = {};
-        //vm.piePostSetupChart;
+        //vm.yearDimensionData = {};
 
         vm.productTypes = {};
         vm.frameworkShortcuts = {};
+
+        //todo: implement locals
+        vm.de_DE = {
+            decimal: ",",
+            thousands: ".",
+            grouping: [3],
+            currency: ["", "\xa0€"]
+        };
+
+        vm.numberFormat = d3.format('04d');
+        vm.currenFormat = d3.format(',.0f');
+        vm.formatNumber = d3.format(".1f");
 
         vm.rowChartOptions = {
             width: 300,
@@ -41,36 +40,23 @@
             x: d3.scale.linear()
         };
 
-        vm.de_DE = {
-            decimal: ",",
-            thousands: ".",
-            grouping: [3],
-            currency: ["", "\xa0€"]
-        };
-
-        var currenFormat = d3.format(',.0f');
-
         vm.rowChartPostSetup = function (c) {
             c.label(function (d) {
                 return d.key + ' ' + (vm.productTypes[d.key] ? vm.productTypes[d.key] : '');
             })
                 .title(function (d) {
-                    return d.key + ': ' + currenFormat(d.value / 1000) + ' Mio.€';
+                    return d.key + ': ' + vm.currenFormat(d.value / 1000) + ' Mio.€';
                 })
                 //.xAxis().tickFormat(formatCurrency)
-                .xAxis().tickFormat(
-                function (v) {
+                .xAxis().tickFormat(function (v) {
                     return v / 1000;
                 })
             ;
             c.xAxis().ticks(5);
-
         };
 
-        var formatNumber = d3.format(".1f");
-
         function formatCurrency(d) {
-            var s = formatNumber(d / 1000);
+            var s = vm.formatNumber(d / 1000);
             //var t = $scope.d3.scale.linear().domain();
             //var t = vm.rowChartOptions.x.domain();
             //var u = d === t[1] ? s + " Mio.€" : s;
@@ -111,10 +97,167 @@
         //
         //};
 
+        vm.ndx = crossfilter();
+        //console.log(vm.ndx);
+
+        vm.yearDimension = vm.ndx.dimension(function (d) {
+            return d.bookingYear;
+        });
+        //console.log(vm.yearDimension.top(4));
+        vm.paymentsSumGroup = vm.yearDimension.group().reduceSum(function (d) {
+            return Math.round(d.paymentsIn / 1000000);
+        });
+
+        vm.paymentsOutSumGroup = vm.yearDimension.group().reduceSum(function (d) {
+            return Math.round(d.paymentsOut / 1000000);
+        });
+
+        //console.log(vm.paymentsSumGroup.top(3));
+        //console.log(vm.paymentsOutSumGroup.top(2));
+
+        vm.prod1Dimension = vm.ndx.dimension(function (d) {
+            return d.productId.substring(0, 1);
+        });
+
+        vm.prod1SumGroup = vm.prod1Dimension.group().reduceSum(function (d) {
+            return Math.round(d.paymentsOut / 1000);
+        });
+
+        vm.prod2Dimension = vm.ndx.dimension(function (d) {
+            return d.productId.substring(0, 2);
+        });
+
+        vm.prod2SumGroup = vm.prod2Dimension.group().reduceSum(function (d) {
+            return Math.round(d.paymentsOut / 1000);
+        });
+
+        vm.prod3Dimension = vm.ndx.dimension(function (d) {
+            return d.productId.substring(0, 3);
+        });
+
+        vm.prod3SumGroup = vm.prod3Dimension.group().reduceSum(function (d) {
+            return Math.round(d.paymentsOut / 1000);
+        });
+
+        //Payment group dimension
+        vm.paymentDimension = vm.ndx.dimension(function (d) {
+            return Math.round(d.amount / 1000);
+        });
+        vm.paymentMax = 200000;
+
+        //console.log(vm.paymentDimension);
+        //vm.paymentGroup = vm.paymentDimension.group(function (d) {
+        //    return d.aumount;
+        //});
+        vm.paymentGroup = vm.paymentDimension.group().reduceSum(function (d) {
+            return Math.round(d.amount / 1000);
+        });
+        //console.log(vm.paymentGroup);
+
+        vm.paymentRangeChartOptions = {
+            // title can be called by any stack layer.
+            title: function (d) {
+                return d.value;
+            }
+        };
+        // Workaround: https://github.com/TomNeyland/angular-dc/issues/15
+        // paymentRageSelectChart has to be defined separately due to an error in angular-dc
+        // The attribute rangeChart can't be set directly in the range chart.
+        vm.paymentRangeSelectChart = dc.barChart("#paymentRangeSelectChart", "1");
+        vm.paymentRangeSelectChart.alwaysUseRounding(true)
+            .width(1100)
+            .height(85)
+            .margins({top: 0, right: 25, bottom: 40, left: 60})
+            .brushOn(true)
+            .elasticX(true) // First get the right size, then it has to be set to false in order to work.
+            .elasticY(true)
+            .centerBar(true)
+            .gap(0)
+            .dimension(vm.paymentDimension)
+            .group(vm.paymentGroup)
+            .outerPadding(0.05)
+            //.renderHorizontalGridLines(true)
+            .xAxisLabel('Zahlungen gruppiert nach Jahr, Produkt, Konto und Betrag in T€')
+            .yAxisLabel(' ')
+            .x(d3.scale.linear().domain([0, vm.paymentMax]))
+            .y(d3.scale.log().domain([0, vm.paymentMax]))
+            .yAxis().ticks(0)
+        ;
+
+        vm.setupRangeChart = function (myRangeChart) {
+            myRangeChart.rangeChart(vm.paymentRangeSelectChart);
+            vm.paymentRangeSelectChart.render();
+        };
+
+        //vm.rowChartProductField = dc.rowChart("#rowChartProductField");
+        //vm.rowChartProductField
+        //    .dimension(vm.prod1Dimension)
+        //    .group(vm.prod1SumGroup)
+        //    .chartGroup("1")
+        //    .width(300)
+        //    .height(200)
+        //    .gap(2)
+        //    .cap(7)
+        //    .othersLabel('Sonstige')
+        //    .elasticX(true)
+        //    .elasticY(true)
+        //    .sortBy(function (d) {return d.amount;})
+        //    .x(d3.scale.linear().domain([0, 2000000]))
+        //;
+        //
+        //vm.rowChartProductField.render();
+
+        // data table does not use crossfilter group but rather a closure
+        // as a grouping function
+        vm.tableGroup = function (d) {
+
+            return d.bookingYear;
+        };
+
+        vm.piePostSetupChart = function (c) {
+            c.label(function (d) {
+                return d.key + ' ' + vm.productTypes[d.key];
+                //return d.key;
+            });
+        };
+
+        vm.tablePostSetupChart = function (c) {
+            // dynamic columns creation using an array of closures
+            c.columns([
+                function (d) {
+                    return d.bookingYear;
+                },
+                function (d) {
+                    return d.productId + ' ' + (vm.productTypes[d.productId] ? vm.productTypes[d.productId] : '');
+                },
+                function (d) {
+                    return d.account + ' ' + (vm.frameworkShortcuts[d.account] ? vm.frameworkShortcuts[d.account] : '');
+                },
+                function (d) {
+                    return '<span style="float:right; padding-right:5px">' + vm.currenFormat(d.amount) + '</span>';
+                }
+            ])
+                // (optional) sort using the given field, :default = function(d){return d;}
+                .sortBy(function (d) {
+                    return d.amount;
+                })
+                // (optional) sort order, :default ascending
+                .order(d3.descending)
+                // (optional) custom renderlet to post-process chart using D3
+                .on('renderlet', (function (table) {
+                    table.selectAll(".dc-table-group").classed("info", true);
+                }));
+
+            //vm.yearDimensionData = vm.yearDimension.top(Infinity);
+        };
+
+        vm.resetAll = function () {
+            dc.filterAll("1");
+            dc.redrawAll("1");
+        };
+
         vm.bookingsData = function (values) {
             vm.loading = true;
-            var numberFormat = d3.format('04d');
-            var currenFormat = d3.format(',.0f');
             //var DE = d3.locale(vm.de_DE);
 
             //console.log(vm.bookingsData[0].personId);
@@ -122,7 +265,7 @@
                 .then(function (results) {
 
                     results.forEach(function (d) {
-                        //d.bookingYear = numberFormat(d.bookingYear);
+                        //d.bookingYear = vm.numberFormat(d.bookingYear);
                         if (d.account.substring(0, 7) === 'NKF05F1') {
                             d.paymentsIn = d.amount;
                             d.paymentsOut = null;
@@ -133,158 +276,12 @@
                     });
                     //console.log('Imported bookings data', results);
 
-                    vm.ndx = crossfilter(results);
+                    vm.ndx.add(results);
                     //console.log(vm.ndx);
-
-                    vm.yearDimension = vm.ndx.dimension(function (d) {
-                        return d.bookingYear;
-                    });
-                    //console.log(vm.yearDimension.top(4));
-                    vm.paymentsSumGroup = vm.yearDimension.group().reduceSum(function (d) {
-                        return Math.round(d.paymentsIn / 1000000);
-                    });
-
-                    vm.paymentsOutSumGroup = vm.yearDimension.group().reduceSum(function (d) {
-                        return Math.round(d.paymentsOut / 1000000);
-                    });
-
-                    //console.log(vm.paymentsSumGroup.top(3));
-                    //console.log(vm.paymentsOutSumGroup.top(2));
-
-                    vm.prod1Dimension = vm.ndx.dimension(function (d) {
-                        return d.productId.substring(0, 1);
-                    });
-
-                    vm.prod1SumGroup = vm.prod1Dimension.group().reduceSum(function (d) {
-                        return Math.round(d.paymentsOut / 1000);
-                    });
-
-                    vm.prod2Dimension = vm.ndx.dimension(function (d) {
-                        return d.productId.substring(0, 2);
-                    });
-
-                    vm.prod2SumGroup = vm.prod2Dimension.group().reduceSum(function (d) {
-                        return Math.round(d.paymentsOut / 1000);
-                    });
-
-                    vm.prod3Dimension = vm.ndx.dimension(function (d) {
-                        return d.productId.substring(0, 3);
-                    });
-
-                    vm.prod3SumGroup = vm.prod3Dimension.group().reduceSum(function (d) {
-                        return Math.round(d.paymentsOut / 1000);
-                    });
-
-                    //Payment group dimension
-                    vm.paymentDimension = vm.ndx.dimension(function (d) {
-                        return Math.round(d.amount / 1000);
-                    });
                     vm.paymentMax = Math.round(vm.paymentDimension.top(1)[0].amount / 1000);
-
-                    //console.log(vm.paymentDimension);
-                    //vm.paymentGroup = vm.paymentDimension.group(function (d) {
-                    //    return d.aumount;
-                    //});
-                    vm.paymentGroup = vm.paymentDimension.group().reduceSum(function (d) {
-                        return Math.round(d.amount / 1000);
-                    });
-                    //console.log(vm.paymentGroup);
-
-                    vm.paymentRangeChartOptions = {
-                        // title can be called by any stack layer.
-                        title: function (d) {
-                            return d.value;
-                        }
-                    };
-
-                    vm.rsc = dc.barChart("#paymentRangeSelectChart" );
-                    vm.rsc.alwaysUseRounding(true)
-                        .width(1100)
-                        .height(85)
-                        .margins({top: 0, right: 0, bottom: 45, left: 60})
-                        .brushOn(true)
-                        .elasticX(true)
-                        .elasticY(true)
-                        .centerBar(true)
-                        .gap(1)
-                        .dimension(vm.paymentDimension)
-                        .group(vm.paymentGroup)
-                        .outerPadding(0.05)
-                        //.renderHorizontalGridLines(true)
-                        .xAxisLabel('Zahlungen gruppiert nach Jahr, Produkt, Konto und Betrag in T€')
-                        .yAxisLabel(' ')
-                        .x(d3.scale.linear().domain([0, vm.paymentMax]))
-                        .yAxis().ticks(0)
-                    ;
-
-                    vm.setupRangeChart = function (myRangeChart) {
-                        myRangeChart.rangeChart(vm.rsc);
-                        vm.rsc.render();
-                    };
-
-                    //vm.rowChartProductField = dc.rowChart("#rowChartProductField");
-                    //vm.rowChartProductField
-                    //    .dimension(vm.prod1Dimension)
-                    //    .group(vm.prod1SumGroup)
-                    //    .chartGroup("1")
-                    //    .width(300)
-                    //    .height(200)
-                    //    .gap(2)
-                    //    .cap(7)
-                    //    .othersLabel('Sonstige')
-                    //    .elasticX(true)
-                    //    .elasticY(true)
-                    //    .sortBy(function (d) {return d.amount;})
-                    //    .x(d3.scale.linear().domain([0, 2000000]))
-                    //;
-                    //
-                    //vm.rowChartProductField.render();
-
-                    // data table does not use crossfilter group but rather a closure
-                    // as a grouping function
-                    vm.tableGroup = function (d) {
-
-                        return d.bookingYear;
-                    };
-
-                    vm.piePostSetupChart = function (c) {
-                        c.label(function (d) {
-                            return d.key + ' ' + vm.productTypes[d.key];
-                            //return d.key;
-                        });
-                    };
-
-                    vm.tablePostSetupChart = function (c) {
-                        // dynamic columns creation using an array of closures
-                        c.columns([
-                            function (d) {
-                                return d.bookingYear;
-                            },
-                            function (d) {
-                                return d.productId + ' ' + (vm.productTypes[d.productId] ? vm.productTypes[d.productId] : '');
-                            },
-                            function (d) {
-                                return d.account + ' ' + (vm.frameworkShortcuts[d.account] ? vm.frameworkShortcuts[d.account] : '');
-                            },
-                            function (d) {
-                                return '<span style="float:right; padding-right:5px">' + currenFormat(d.amount) + '</span>';
-                            }
-                        ])
-                            // (optional) sort using the given field, :default = function(d){return d;}
-                            .sortBy(function (d) {
-                                return d.amount;
-                            })
-                            // (optional) sort order, :default ascending
-                            .order(d3.descending)
-                            // (optional) custom renderlet to post-process chart using D3
-                            .on('renderlet', (function (table) {
-                                table.selectAll(".dc-table-group").classed("info", true);
-                            }));
-
-                        //vm.yearDimensionData = vm.yearDimension.top(Infinity);
-                    };
-
-
+                    vm.resetAll();
+                    // Reset elasticX to false, otherwise rage chart won't work.
+                    $scope.paymentRangeChart.elasticX(false);
                 })
                 .catch(function (err) {
                     console.log('Error Imported bookings data', err);
@@ -293,35 +290,5 @@
                     vm.loading = false;
                 });
         };
-
-        //vm.chart = dc.barChart("#test");
-        //
-        //vm.counts = [
-        //    {name: "apple", cnt: 10},
-        //    {name: "orange", cnt: 15},
-        //    {name: "banana", cnt: 12},
-        //    {name: "grapefruit", cnt: 2},
-        //    {name: "grapefruit", cnt: 4}
-        //];
-        //
-        //vm.ndx2            = crossfilter(vm.counts);
-        //vm.fruitDimension = vm.ndx2.dimension(function(d) {return d.name;});
-        //vm.sumGroup       = vm.fruitDimension.group().reduceSum(function(d) {return d.cnt;});
-        //
-        //vm.chart
-        //    .width(768)
-        //    .height(380)
-        //    .x(d3.scale.ordinal())
-        //    .xUnits(dc.units.ordinal)
-        //    .brushOn(false)
-        //    .xAxisLabel("Fruit")
-        //    .yAxisLabel("Quantity Sold")
-        //    .dimension(vm.fruitDimension)
-        //    .barPadding(0.1)
-        //    .outerPadding(0.05)
-        //    .group(vm.sumGroup);
-        //
-        //vm.chart.render();
-
     }
 })();
