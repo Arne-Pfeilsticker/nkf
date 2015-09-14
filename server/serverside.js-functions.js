fd573752140d446732a9e35f701355da04bacb55
Arne Pfeilsticker <Arne.Pfeilsticker@piratenpartei-hessen.de>
2015-09-15 01:48:39 +0200

    create serverside js functions

--- server/serverside.js-functions.js
+++ server/serverside.js-functions.js
@@ -1,5 +1,5 @@
 /**
- * Definition of the server side functions
+ * Definition of the server side js functions
  */
 
 module.exports = (function () {
@@ -29,114 +29,6 @@ module.exports = (function () {
     var db = server.use(odb.DB);
     console.log('Using database: ' + db.name);
 
- //   db.class.list()
- //       .then(function (results) {
- //           console.log('Existing Classes:', results);
- //           return db.class.create('TestClass');
- //       })
- //       .then(function (results) {
- //           console.log('Created Class:', results);
- //           return db.class.delete('TestClass');
- //       })
- //       .then(function (results) {
- //           console.log('Deleted Class');
- ////           process.exit();
- //       })
- //       .done();
-
-    var fnName = '';
-
-    /**
-     * Create a OrientDB server side function from a plain Javascript function
-     *
-     * @param {String}  language    The language of the function [Javascript | SQL]
-     * @param {Boolean} idempotent  [true|false]
-     * @param {Object}  fn          Plain Javascript function to stringify
-     *
-     * This function ist only beause Orientjs's createFn can't set parameter idempotent
-     */
-    var createServerside = function (language, idempotent, fn) {
-
-        var lang = ' LANGUAGE ' + language.toLowerCase();
-        var idem = ' IDEMPOTENT ' + (idempotent ? 'true' : 'false');
-
-        var fnDef = parseFn(fn);
-        var params = "";
-        fnName = fnDef.name;
-        var body = fnDef.body;
-
-        if (language == 'sql') {
-            eval(body);
-            body = cmd; // cmd variable contains the sql command
-        } else {
-            body.replace(/\'/g, "\\'")
-                .replace(/\"/g, '\\"')
-                .replace(/( {8})/g, '')
-                .trim();
-        }
-
-        if (fnDef.arguments.length > 0) {
-            params = 'PARAMETERS [' + fnDef.params + ']';
-        }
-
-        var fnExists = false;
-
-        var createFunction = 'CREATE FUNCTION ' + fnName + ' \"' + body + '\"' + params + lang + idem;
-
-        // Delete function if exists.
-        //
-        //db.select('name')
-        //    .from('OFunction')
-        //    .where({name: fnName})
-        //    .limit(1)
-        //    .one()
-        //    .then(function (params) {
-        //        if (params.name == fnName) {
-        //            console.log('Function ' + params.name + ' exists.');
-        //            fnExists = true;
-        //        }
-        //    })
-        //    .catch(function (err) {
-        //        console.log('Info: function ' + fnName + ' does not exist.');
-        //        fnExists = false;
-        //    })
-        //    .done();
-        //
-        //if (fnExists) {
-        //    db.delete()
-        //        .from('OFunction')
-        //        .where({name: fnName})
-        //        .scalar()
-        //        .then(function (total) {
-        //            console.log('Deleted', total, 'function');
-        //        })
-        //        .done();
-        //}
-
-        //db.insert().into('OUser').set({name: 'demo', password: 'demo', status: 'ACTIVE'}).one()
-        //    .then(function (user) {
-        //        console.log('created', user);
-        //    });
-
-        db.exec(createFunction)
-            .then(function (response) {
-                if (response.status.code == 0) {
-                    console.log('Function ' + fnName + ' created successfully.');
-                } else {
-                    console.error('Error: Create function ' + fnName + ' faild.');
-                    exit(1);
-                }
-            })
-            .catch(function (err) {
-                console.log('Error: Create function ' + fnName + ' faild.');
-                console.log(err);
-            })
-            .done();
-    };
-
-    var js = 'javascript',
-        sql = 'sql';
-
     // Here the definition of server side functions begins.
 
     db.createFn(function getAllUser() {
@@ -148,19 +40,6 @@ module.exports = (function () {
         console.log("Function getAllUser created. Count: " + count)
     });
 
-    createServerside(sql, true, function getFramework() {
-        var cmd = "select $depth as treeLevel, out('hasSubaccount').size() as subaccounts, id, parent_id, label, shortcut, sign, breakdown, beneficiary, provider  from (traverse out('hasSubaccount') from (select from Framework where id = 'NKF05'))";
-    });
-
-    createServerside(sql, true, function persons_getTypes() {
-        var cmd = "select $depth as treeLevel, out('hasPersonSubtype').size() as personsubtypes, id, parent_id, label, acronym from (traverse out('hasPersonSubtype') from (select from PersonTypes where id = 'pt'))";
-    });
-
-    createServerside(sql, true, function persons_getAll() {
-        var cmd = "select $depth as treeLevel, out('hasPersons').size() as personsubtypes, id, parent_id, name, person_type, wiki_url, begin, end from (traverse out('hasPersons') from (select from Persons where id = 'de'))";
-    });
-
-    //createServerside(js, true, function importBookings() {
     db.createFn(function importBookings() {
         /**
          * Insert Bookings and add edges to timeline and person
